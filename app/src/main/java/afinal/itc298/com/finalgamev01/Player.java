4bc545d658e206771c876e83dca0aec5ffb04d30
jayke <jaykel123abc@gmail.com>
2017-07-12 21:36:57 -0700

    Added an enemy Created abstract method in GameObject Rect getBounds(); Added collision method to Player (still needs some work)

--- app/src/main/java/afinal/itc298/com/finalgamev01/Player.java
+++ app/src/main/java/afinal/itc298/com/finalgamev01/Player.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
+import android.support.constraint.solver.widgets.Rectangle;
 
 /**
  * Created by jayke on 7/11/2017.
@@ -14,13 +15,17 @@ public class Player extends GameObject {
 
     Paint paint = new Paint();
     Rect rect = new Rect(10,10,100,100);
+    Handler handler;
 
-    public Player(int x, int y, ID id) {
+    public Player(int x, int y, ID id, Handler handler) {
         super(x, y, id);
-
-
+        this.handler = handler;
 
         // TODO Auto-generated constructor stub
+    }
+
+    public Rect getBounds() {
+        return new Rect(rect);
     }
 
     @Override
@@ -28,9 +33,23 @@ public class Player extends GameObject {
         x+=velX;
         y+=velY;
 
-        x = GamePanel.clamp(x, 50, Constants.SCREEN_WIDTH-50);//cannot go beyond left and right border
+        if(x < 0) x = Constants.SCREEN_WIDTH ;//if traveling to the left end of the screen appear on the right end
+        if(x > Constants.SCREEN_WIDTH ) x = 0;//if traveling to the right end of the screen appear on the left end
         y = GamePanel.clamp(y, 50, Constants.SCREEN_HEIGHT-50);//cannot go beyond top and bottom border
 
+        collision();
+
+    }
+
+    private void collision(){
+        for(int i = 0; i < handler.object.size(); i++){
+            GameObject tempObject = handler.object.get(i);
+            if(tempObject.getId() == ID.BasicEnemy){
+                if(getBounds().intersect(tempObject.getBounds())){
+                    //tempObject.velX = velX*-1;//WORK ON COLLISION, HALF WORKS
+                }
+            }
+        }
     }
 
     @Override
@@ -45,6 +64,7 @@ public class Player extends GameObject {
         paint.setColor(Color.BLACK);
         canvas.drawRect(rect, paint);
 
+        //position middle of square
         rect.set((int)(x - rect.width()/2), (int)(y - rect.height()/2),(int)(x + rect.width()/2),(int)(y + rect.height()/2));
 
 
